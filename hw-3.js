afb99cc8821d7fd97fd3545d64b6c74dfff316b3
AnnaM-7777777 <anna.morozova7777777@yandex.ru>
2025-06-09 12:36:22 +0300

    Выполнено Задание 1

---
+++ hw-3.js
@@ -0,0 +1,12 @@
+//_______________________ Задание 1 _______________________
+
+let password = 'пароль';
+let whichPassword = prompt('Введите пароль');
+
+if (whichPassword === password) {
+     console.log('Пароль введен верно');
+
+} else {
+    console.log('Пароль введен неправильно');
+}
+
