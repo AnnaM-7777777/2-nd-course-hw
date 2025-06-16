1e1e026e06865ec3932d81de20b02e0e4a165c9d
AnnaM-7777777 <anna.morozova7777777@yandex.ru>
2025-06-16 15:49:50 +0300

    Выполнено Задание 2

--- hw-5.js
+++ hw-5.js
@@ -15,3 +15,19 @@ console.log(smallerNumber(6, 4));
 console.log(smallerNumber(3, 3));
 
 
+//_______________________ Задание 2 _______________________
+
+function evenOddNumber(number) {
+
+    if (number % 2 === 0) {
+        return console.log('Число четное');
+
+    } else {
+        return console.log('Число нечетное');
+    }
+}
+
+evenOddNumber(28);
+evenOddNumber(7);
+
+
