9154074edead25ea9bcae1019d37a1af039db679
AnnaM-7777777 <anna.morozova7777777@yandex.ru>
2025-06-16 15:49:09 +0300

    Выполнено Задание 1

---
+++ hw-5.js
@@ -0,0 +1,17 @@
+//_______________________ Задание 1 _______________________
+
+function smallerNumber (a, b) {
+
+    if (a < b) {
+        return a;
+
+    } else {
+        return b;
+    }
+}
+
+console.log(smallerNumber(2, 5));
+console.log(smallerNumber(6, 4));
+console.log(smallerNumber(3, 3));
+
+
