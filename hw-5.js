f429d5eb1b32852c6bc7f8270c2631dba7ef148e
AnnaM-7777777 <anna.morozova7777777@yandex.ru>
2025-06-23 22:40:57 +0300

    Исправлены недочеты

--- hw-5.js
+++ hw-5.js
@@ -1,13 +1,7 @@
 //_______________________ Задание 1 _______________________
 
 function smallerNumber (a, b) {
-
-    if (a < b) {
-        return a;
-
-    } else {
-        return b;
-    }
+    return a < b ? a : b;
 }
 
 console.log(smallerNumber(2, 5));
@@ -18,10 +12,8 @@ console.log(smallerNumber(3, 3));
 //_______________________ Задание 2 _______________________
 
 function evenOddNumber(number) {
-
     if (number % 2 === 0) {
         return console.log('Число четное');
-
     } else {
         return console.log('Число нечетное');
     }
@@ -34,7 +26,6 @@ evenOddNumber(7);
 //_______________________ Задание 3 _______________________
 
 function squareNumber1(number) {
-
     const power = number ** 2;
     console.log(power);
 }
@@ -54,15 +45,12 @@ squareNumber2(2);
 //_______________________ Задание 4 _______________________
 
 function howOld() {
-
-    let age = Number(prompt("Сколько Вам лет?"));
+    const age = Number(prompt("Сколько Вам лет?"));
 
     if (age < 0 || isNaN(age)) {
         console.log('Вы ввели неправильное значение!');
-
     } else if (age >= 0 && age < 13) {
         console.log('Привет, друг!');
-
     } else {
         console.log('Добро пожаловать!')
     }
@@ -74,54 +62,44 @@ howOld();
 //_______________________ Задание 5 _______________________
 
 function checkingNumbers() {
-
-    const num1 = prompt('Введите первое число');
-    const num2 = prompt('Введите второе число');
-
+    const num1 = Number(prompt('Введите первое число'));
+    const num2 = Number(prompt('Введите второе число'));
 
     if (isNaN(num1) || isNaN(num2)) {
         return console.log('Одно или оба значения не являются числом');
-
     } else {
         return console.log(num1 * num2);
     }
 }
 
- checkingNumbers();
+checkingNumbers();
 
 
 //_______________________ Задание 6 _______________________
 
-
 function powerNumber() {
-
     const input = prompt('Введите число');
 
     if (isNaN(input)) {
         return console.log('Переданный параметр не является числом');
-
     } else {
         return console.log(`${input} в кубе равняется`, input ** 3);
     }
 }
 
- powerNumber();
+powerNumber();
 
 
 //_______________________ Задание 7 _______________________
 
 const circle1 = {
     radius: 7,
-    color: 'blue',  
-
+    color: 'blue',
     getArea() {
-
-            return Math.PI * this.radius ** 2;
+        return Math.PI * this.radius ** 2;
     },
-
     getPerimeter() {
-
-            return 2 * Math.PI * this.radius;
+        return 2 * Math.PI * this.radius;
     }
 };
 
@@ -132,15 +110,11 @@ console.log(`Периметр окружности:`, circle1.getPerimeter());
 const circle2 = {
     radius: 12,
     color: 'green',  
-
     getArea() {
-
-            return Math.PI * this.radius ** 2;
+        return Math.PI * this.radius ** 2;
     },
-
     getPerimeter() {
-
-            return 2 * Math.PI * this.radius;
+        return 2 * Math.PI * this.radius;
     }
 };
 
