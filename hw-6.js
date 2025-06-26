5b8b0dd0ccb825a396f7d43c92f5dc09c0bef39f
AnnaM-7777777 <anna.morozova7777777@yandex.ru>
2025-06-26 17:10:05 +0300

    Выполнено Задание 2

--- hw-6.js
+++ hw-6.js
@@ -10,3 +10,16 @@ for (let i = 0; i < numbers.length; i++) {
 }
 
 
+//_______________________ Задание 2 _______________________
+
+let arr = [1, 5, 4, 10, 0, 3];
+let val = 4;
+
+for (let i = 0; i < arr.length; i++) {
+	if (arr[i] === val) {
+		console.log(i);
+        break;
+	}
+}
+
+
