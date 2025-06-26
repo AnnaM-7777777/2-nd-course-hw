12d9b2640f53a6c7fd6d393134bab8ea4417f30f
AnnaM-7777777 <anna.morozova7777777@yandex.ru>
2025-06-26 17:51:04 +0300

    Выполнено Задание 4

--- hw-6.js
+++ hw-6.js
@@ -22,8 +22,25 @@ for (let i = 0; i < arr.length; i++) {
 	}
 }
 
+
 //_______________________ Задание 3 _______________________
 
 let num = [1, 3, 5, 10, 20];
 let joinNum = num.join(" ");
+
 console.log(joinNum);
+
+
+//_______________________ Задание 4 _______________________
+
+let arrays = [
+    [1, 1, 1], 
+    [2, 2, 2], 
+    [3, 3, 3]
+];
+
+for (let item of arrays) {
+    for (let i = 0; i < item.length; i++) {
+        console.log(item);
+    }
+}
