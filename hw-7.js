1735e1fb2fcfb2d49bee757380043db0d73a1dc7
AnnaM-7777777 <anna.morozova7777777@yandex.ru>
2025-07-02 15:24:02 +0300

    Выполнено Задание 2

--- hw-7.js
+++ hw-7.js
@@ -3,3 +3,20 @@
 const str = 'js';
 console.log(str.toUpperCase());
 
+
+//_______________________ Задание 2 _______________________
+
+const arrFruits = ["Lemon", "pear", "orange", "apple", "apriCot", "peach", "Pineapple", "banAna", "strawberrY", "cherry"];
+const strPrefix = "p";
+
+function filterByPrefix(arrFruits, strPrefix) {
+    strPrefix = strPrefix.toLowerCase();
+
+    return arrFruits.filter(function(item) {
+        return item.toLowerCase().startsWith(strPrefix);
+    });
+}
+
+console.log(filterByPrefix(arrFruits, strPrefix));
+
+
