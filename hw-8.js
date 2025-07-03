4cdafbb7b540c33cbc8f85970c067b04b4ea2911
AnnaM-7777777 <anna.morozova7777777@yandex.ru>
2025-07-04 02:58:38 +0300

    Выполнено Задание 2

--- hw-8.js
+++ hw-8.js
@@ -11,4 +11,35 @@ people.sort((a, b) => a.age - b.age);
 console.log(people.map(person => `${person.name}: ${person.age}`));
 
 
+//_______________________ Задание 2 _______________________
+
+function isPositive(number) {
+    return number > 0;
+}
+
+function isMale(human) {
+    return human.gender === 'female';
+}
+
+function filter(arr, ruleFunction) {
+    const output = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (ruleFunction(arr[i])) {
+            output.push(arr[i]);
+        }
+    }
+    return output;
+}
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const menAndWomen = [
+    {name: 'Глеб', gender: 'male'},
+    {name: 'Анна', gender: 'female'},
+    {name: 'Олег', gender: 'male'},
+    {name: 'Оксана', gender: 'female'}
+];
+console.log(menAndWomen.filter(isMale).map(human => `${human.name}: ${human.gender}`));
+
+
 
