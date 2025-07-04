2cdedd4bd7ca5b93733ab6130d9118e0fc913429
AnnaM-7777777 <anna.morozova7777777@yandex.ru>
2025-07-05 00:28:56 +0300

    Устранена ошибка в консоле при отмене игры

--- hw-8-game-4.js
+++ hw-8-game-4.js
@@ -1,4 +1,4 @@
-//_______________________ Простая викторина _______________________
+//_______________________ Камень, ножницы, бумага _______________________
 
 function showPopupGame4() {
     document.getElementById('popupGame4').style.display = 'block';
@@ -29,33 +29,26 @@ function determineWinner(userVersion, computerVersion) {
     }
 }
 
-function determineWinner(userVersion, computerVersion) {
-    if (userVersion === computerVersion) {
-        return 'Ничья!';
-    } else if (
-        (userVersion === 'камень' && computerVersion === 'ножницы') ||
-        (userVersion === 'ножницы' && computerVersion === 'бумага') ||
-        (userVersion === 'бумага' && computerVersion === 'камень')
-    ) {
-        return 'Победа!';
-    } else {
-        return 'Поражение!';
-    }
-}
-
-
 function playGame() {
-    const userVersion = prompt('Выберите: камень, ножницы или бумага').toLowerCase();
+    const userVersion = prompt('Выберите: камень, ножницы или бумага');
 
-    if (!['камень', 'ножницы', 'бумага'].includes(userVersion)) {
+    // Проверяем, не была ли нажата кнопка "Отмена" в prompt
+    if (userVersion === null) {
+        alert('Игра отменена.');
+        return; // Прерываем выполнение функции
+    }
+    
+    const userVersionLower = userVersion.toLowerCase();
+    
+    if (!['камень', 'ножницы', 'бумага'].includes(userVersionLower)) {
         alert('Некорректный ввод. Пожалуйста, выберите камень, ножницы или бумага.');
         return;
     }
 
     const computerVersion = getComputerChoice();
     const resultElement = document.getElementById('resultGame'); // Получаем ссылку на элемент, куда будем выводить результат
-    const result = determineWinner(userVersion, computerVersion);
-    resultElement.innerText = `Вы выбрали: ${userVersion}. \n Компьютер выбрал: ${computerVersion}. \n Результат: ${result}`;
+    const result = determineWinner(userVersionLower, computerVersion);
+    resultElement.innerText = `Вы выбрали: ${userVersionLower}. \n Компьютер выбрал: ${computerVersion}. \n Результат: ${result}`;
 }
 
 const playButton = document.getElementById('playButton');
